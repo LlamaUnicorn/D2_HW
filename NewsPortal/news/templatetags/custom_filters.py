190a42238215b4f22dcb85bb36a4a20df6dddc62
LlamaUnicorn <holidaynight@gmail.com>
2022-04-12 20:49:38 +0300

    D3_HW: censor almost done. TODO: Add type exception

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -3,16 +3,10 @@ from django import template
 register = template.Library()
 
 
-# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
-# что это именно фильтр для шаблонов, а не простая функция.
 @register.filter()
 def censor(value):
-    """
-   value: значение, к которому нужно применить фильтр
-   """
-    censored = ['новости']
-    for value in censored:
-        censored_word = '*' * len(value)
-    return censored_word
-
-    # Возвращаемое функцией значение подставится в шаблон.
+    profanity = ['соня', 'дом', 'редиска']
+    for i in profanity:
+        if i.find(value):
+            value = value.replace(i[1::], "*" * (len(i)-1))
+    return f'{value}'
