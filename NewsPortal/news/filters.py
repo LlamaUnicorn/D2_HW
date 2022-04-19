c8de6686409e1142dc328b7f729e70702934d5c9
LlamaUnicorn <holidaynight@gmail.com>
2022-04-19 20:01:31 +0300

    D4_HW: added calendar filtering. Seems like it's not implemented properly.

--- NewsPortal/news/filters.py
+++ NewsPortal/news/filters.py
@@ -1,5 +1,8 @@
 from django_filters import FilterSet, DateFilter
 from .models import Post
+# import django_filters
+# from django.db import models
+from django import forms
 
 
 # Создаем свой набор фильтров для модели Product.
@@ -7,34 +10,10 @@ from .models import Post
 # должен чем-то напомнить знакомые вам Django дженерики.
 
 
-class PostFilter(FilterSet):
-        # time_in = DateFilter(
-        #     lookup_expr='gt',
-        #     widget=django.forms.DateInput(
-        #         attrs={
-        #             'type': 'date'
-        #         }
-        #     )
-        # )
-
-    class Meta:
-        # В Meta классе мы должны указать Django модель,
-        # в которой будем фильтровать записи.
-        model = Post
-        # В fields мы описываем по каким полям модели
-        # будет производиться фильтрация.
-        fields = {
-            # поиск по названию
-            'title': ['icontains'],
-            'dateCreation': ['lt', 'gt'],
-            'postCategory': ['exact'],
-        }
-
-
 # class PostFilter(FilterSet):
 #     time_in = DateFilter(
 #         lookup_expr='gt',
-#         widget=django.forms.DateInput(
+#         widget=forms.DateInput(
 #             attrs={
 #                 'type': 'date'
 #             }
@@ -42,10 +21,34 @@ class PostFilter(FilterSet):
 #     )
 #
 #     class Meta:
+#         # В Meta классе мы должны указать Django модель,
+#         # в которой будем фильтровать записи.
 #         model = Post
+#         # В fields мы описываем по каким полям модели
+#         # будет производиться фильтрация.
 #         fields = {
-#             'heading': ['icontains'],
-#             'author__name': ['exact'],
-#             'category': ['exact'],
-#
+#             # поиск по названию
+#             'title': ['icontains'],
+#             'dateCreation': ['lt', 'gt'],
+#             'postCategory': ['exact'],
 #         }
+
+
+class PostFilter(FilterSet):
+    dateCreation = DateFilter(
+        lookup_expr='gt',
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date'
+            }
+        )
+    )
+
+    class Meta:
+        model = Post
+        fields = {
+            # поиск по названию
+            'title': ['icontains'],
+            'dateCreation': ['lt', 'gt'],
+            'postCategory': ['exact'],
+        }
